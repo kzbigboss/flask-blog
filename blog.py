c5f2bca06e32fbfb8916efe1357d728ff7a972a3
Mark Kazzaz <markkazzaz@gmail.com>
2017-09-20 18:52:05 -0700

    something is wrong

--- blog.py
+++ blog.py
@@ -8,6 +8,9 @@ import sqlite3
 
 ## configuration
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = '9743366228'
 
 app = Flask(__name__)
 
@@ -18,13 +21,26 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-@app.route('/')
+@app.route('/', methods = ['GET','POST'])
 def login():
-    return render_template('login.html')
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSOWRD']:
+            error = 'Invalid credentials.  Try again.'
+        else:
+            session['logged in'] = True
+            return redirect(url_for('main'))
+    return render_template('login.html', error=error)
 
 @app.route('/main')
 def main():
     return render_template('main.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
