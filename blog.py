56d6389c8f7856e62828c907482c77afb20cd9f6
Mark Kazzaz <markkazzaz@gmail.com>
2017-09-21 12:53:06 -0700

    protecting main

--- blog.py
+++ blog.py
@@ -6,6 +6,8 @@ from flask import Flask, render_template, request, session, \
 
 import sqlite3
 
+from functools import wraps
+
 ## configuration
 DATABASE = 'blog.db'
 USERNAME = 'admin'
@@ -21,6 +23,16 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
 @app.route('/', methods = ['GET','POST'])
 def login():
     error = None
@@ -33,6 +45,7 @@ def login():
     return render_template('login.html', error=error)
 
 @app.route('/main')
+@login_required
 def main():
     return render_template('main.html')
 
